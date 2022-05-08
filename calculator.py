fba59a7e5fe296436505ddd47c7a38b5237ea383
JulijaKlimeca <101725848+JulijaKlimeca@users.noreply.github.com>
2022-05-08 20:46:49 +0300

    update calc

--- calculator.py
+++ calculator.py
@@ -28,7 +28,7 @@ while True:
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
  
-        else choice == '3':
+        elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
         
         
