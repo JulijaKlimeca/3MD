c0fbb4dba640a72d307d997d018e12b7d9fb0f9d
JulijaKlimeca <101725848+JulijaKlimeca@users.noreply.github.com>
2022-05-08 19:50:02 +0300

    Updated code
    Updated code

--- calculator.java
+++ calculator.java
@@ -1,40 +1,27 @@
-
 public class Calculator {
-//no-arg constructor
-public Calculator() {
-}
-/**
-* Sum method.
-*/
-public int add(int a, int b) {
-return a + b;
-}
-/**
-* Subtract method.
-*/
-public int subtract(int a, int b) {
-return a - b;
-}
+    public Calculator() {
 
-/**
-* Multiply method.
-*/
-public long multiply(int a, int b) {
-return a * b;
-}
-
-/**
-* Divide method.
-* Note that this method throws an exception when
-* b is zero.
-*/
-public double divide(int a, int b) {
-double result;
-if (b == 0) {
-throw new IllegalArgumentException("Divisor cannot divide by zero");
-} else {
-result = Double.valueOf(a)/Double.valueOf(b);
-}
-return result;
+    }
+    public int add(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public long multiply(int a, int b) {
+        return a * b;
+    }
+    
+    
+    public double divide(int a, int b) {
+        double result;
+        if (b == 0) {
+        throw new IllegalArgumentException("Divisor cannot divide by zero");
+    } else {
+        result = Double.valueOf(a)/Double.valueOf(b);
+    }
+    return result;
 }
 }
